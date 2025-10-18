bd399bc0fe63447b4b1085bef4fd624c6a40e568
wamache <benardwamache@gmail.com>
2025-10-18 04:43:11 +0300

    made changes in the dbphp file added freedatabase credentials

--- config/db.php
+++ config/db.php
@@ -1,4 +1,3 @@
-
 <?php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
@@ -23,5 +22,4 @@ if ($conn->connect_error) {
 } else {
     // echo "Connected to database successfully!";
 }
-?>
 
