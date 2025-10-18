6f075c38a079c59eddc6272e73584fb676545b13
wamache <benardwamache@gmail.com>
2025-10-18 03:54:38 +0300

    made changes in the dbphp file

--- config/db.php
+++ config/db.php
@@ -1,5 +1,10 @@
+
 <?php
-$host = 'localhost';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$host = 'host.docker.internal';
 $db = 'dbexhibition';
 $user = 'root';
 $pass = '';
@@ -8,5 +13,8 @@ $conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+} else {
+    // echo "Connected to database successfully!";
 }
 ?>
+
