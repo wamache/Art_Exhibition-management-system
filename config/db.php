6ef4a3ada497c22d533b14d92bd3d96f7766ef9b
wamache <benardwamache@gmail.com>
2025-10-18 04:18:23 +0300

    made changes in the dbphp file added freedatabase credentials

--- config/db.php
+++ config/db.php
@@ -4,10 +4,17 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$host = 'host.docker.internal';
-$db = 'dbexhibition';
-$user = 'root';
-$pass = '';
+// $host = 'host.docker.internal';
+// $db = 'dbexhibition';
+// $user = 'root';
+// $pass = '';
+
+$host = 'sql7.freesqldatabase.com';
+$db = 'sql7803545';
+$user = 'sql7803545';
+$pass = 'SDwxx86F9Z';
+
+
 
 $conn = new mysqli($host, $user, $pass, $db);
 
